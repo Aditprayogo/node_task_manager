bf468c31648818dc5b142f2c51f2fc56c49f18f8
Aditprayogo <adit.ihzar@gmail.com>
2020-02-28 14:10:45 +0700

    adding task api

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,13 +6,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-
-
-
-// work.save()
-//     .then(work => console.log(work))
-//     .catch(err => console.log(err))
-
-// person.save()
-//     .then(person => console.log(person))
-//     .catch(err => console.log(err))
